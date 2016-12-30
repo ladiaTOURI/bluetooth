906af11cfca47ac15840b670788f9cee159b60b5
ladiaTOURI <touri.lydia@gmail.com>
2016-12-30 16:08:09 +0100

    .

--- app/src/main/java/bluetooth/android/mbed/com/bluetooth/MainActivity.java
+++ app/src/main/java/bluetooth/android/mbed/com/bluetooth/MainActivity.java
@@ -1,4 +1,7 @@
 package bluetooth.android.mbed.com.bluetooth;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -11,14 +14,30 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
-public class MainActivity extends Activity  {
-    Button b1,b2,b3,b4;
+import static android.provider.Telephony.Carriers.NAME;
+
+public class MainActivity extends Activity {
+    private UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    Button b1, b2, b3, b4;
     private BluetoothAdapter BA;
-    private Set<BluetoothDevice>pairedDevices;
+    private Set<BluetoothDevice> pairedDevices;
     ListView lv;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -27,32 +46,37 @@ public class MainActivity extends Activity  {
         setContentView(R.layout.activity_main);
 
         b1 = (Button) findViewById(R.id.button);
-        b2=(Button)findViewById(R.id.button2);
-        b3=(Button)findViewById(R.id.button3);
-        b4=(Button)findViewById(R.id.button4);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        b4 = (Button) findViewById(R.id.button4);
 // classe BluetoothAdapter nous permetrra  de savoir si l'appareil possede la technologie bleutooth
         //grace  a la methode getDefaultAdapter
         BA = BluetoothAdapter.getDefaultAdapter();
-        lv = (ListView)findViewById(R.id.listView);
+        lv = (ListView) findViewById(R.id.listView);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-//activer bluetooth avec la methode startActivityForResult avec l'intent l'ACTION_REQUEST_ENABLE
+
+    //activer bluetooth avec la methode startActivityForResult avec l'intent l'ACTION_REQUEST_ENABLE
     //  dans  ce  cas on demande à l'utilisateur  de l'activer, on l'active pas directement
-    //on peut verifier s'il est  ectiver avec la methode isEnabled
-    public void on(View v){
+    //on peut verifier s'il est  activer avec la methode isEnabled
+    public void on(View v) {
         if (!BA.isEnabled()) {
             Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnOn, 0);
-            Toast.makeText(getApplicationContext(), "Allumé",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Allumez", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
         }
     }
+
     //activer le  bluetooth directement
     /*if (!BA.isEnabled()) {
         BA.enable();
     }*/
 //  savoir si l'utilisateur  a  activer le  bluetooth
-   //@Override
+    //@Override
   /*  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
@@ -64,29 +88,146 @@ public class MainActivity extends Activity  {
         }
     }*/
 // methode pour désactiver le  bluettooth avec disable
-    public void off(View v){
+    public void off(View v) {
         BA.disable();
-        Toast.makeText(getApplicationContext(), "Éteindre" ,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Éteindre", Toast.LENGTH_LONG).show();
     }
+
     // permettre aux   d'autres appareil de  se  connecter avec la méthode   startActivityForResult
     //  startActivityForResult  possede comme valeur ACTION_REQUEST_DISCOVERABLE
-    public  void visible(View v){
+    public void visible(View v) {
         Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
         startActivityForResult(getVisible, 0);
     }
 
-//  découvrir les différents  appreils  connecter  a  note  apppareil  cible  avec la methode getBondedDevices
-    public void list(View v){
+    //  découvrir les différents  appreils  connecter  a  note  apppareil  cible  avec la methode getBondedDevices
+    public void list(View v) {
         pairedDevices = BA.getBondedDevices();
 
         ArrayList list = new ArrayList();
         //dans ce cas on veux  parcourir la list la  list des appareils trouvés
 
-        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
-        Toast.makeText(getApplicationContext(), "Affichage des périphériques couplés",Toast.LENGTH_SHORT).show();
+        for (BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Affichage des périphériques couplés", Toast.LENGTH_SHORT).show();
 
-        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
 
         lv.setAdapter(adapter);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+  /*  private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+            try {
+                tmp = BA.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) {
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+
+                if (socket != null) {
+                    manageConnectedSocket(socket);
+                    mmServerSocket.close();
+                    break;
+                }
+            }
+        }
+
+        private void manageConnectedSocket(BluetoothSocket socket) {
+        }
+
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }*/
+//
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+// configurer la connexion Bluetooth côté client :
+        public ConnectThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            BA.cancelDiscovery();
+            try {
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                }
+                return;
+            }
+            manageConnectedSocket(mmSocket);
+        }
+
+        private void manageConnectedSocket(BluetoothSocket mmSocket) {
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+
     }
 }
